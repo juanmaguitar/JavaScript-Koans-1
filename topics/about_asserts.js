423f09e20e30ecba629f5d3368ac1877e4eade79
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2015-12-06 10:25:15 +0100

    livereload

--- topics/about_asserts.js
+++ topics/about_asserts.js
@@ -2,11 +2,11 @@
 module("About Asserts (topics/about_asserts.js)");
 
 test("ok", function() {
-    ok(__ === true, 'what will satisfy the ok assertion?');
+    ok( __ === true, 'what will satisfy the ok assertion?');
 });
 
 test("not ok", function() {
-    ok(__ === false, 'what is a false value?');
+    ok( __ === false, 'what is a false value?');
 });
 
 test("equal", function() {
